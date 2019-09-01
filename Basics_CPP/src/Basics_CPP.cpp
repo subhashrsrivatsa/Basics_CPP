2cbebca093b5ff7527f74f1680b92c9ecd7833f1
chinni <chinni@DESKTOP-88OO4MJ>
2019-09-01 14:11:26 +0200

    Two more methods Sizes - The sizes of different variables on my PC Characters - Character set in the PC keyboard

--- Basics_CPP/src/Basics_CPP.cpp
+++ Basics_CPP/src/Basics_CPP.cpp
@@ -18,16 +18,36 @@ void Calculator()
 {
 	int a=8,b=3;
 	cout<<"\n\nSample Calculator DEMO\n";
-	cout<<"SUM of "<<a<<" and "<<b<<" is : \t"<<(a+b);
+	cout<<"SUM of "<<a<<" and "<<b<<" is : \t\t"<<(a+b);
 	cout<<"\nDIFFERENCE of "<<a<<" and "<<b<<" is : \t"<<(a-b);
 	cout<<"\nPRODUCT of "<<a<<" and "<<b<<" is : \t"<<(a*b);
-	cout<<"\nDIFFERENCE of "<<a<<" and "<<b<<" is : \t"<<((float)(a/b));
+	cout<<"\nDIFFERENCE of "<<a<<" and "<<b<<" is : \t"<<(a/b);
 
+}
+
+void Sizes()
+{
+	cout<<"\n\nThe sizes of different variables on my PC\n";
+	cout<<"int : \t"<<sizeof(int);
+	cout<<"\nlong : \t"<<sizeof(long);
+	cout<<"\nchar : \t"<<sizeof(char);
+	cout<<"\ndouble : "<<sizeof(double);
+	cout<<"\nfloat : "<<sizeof(float);
+}
+void Characters()
+{
+	cout<<"\nThe Character set in the PC keyboard are\n";
+	for(int i=32;i<128;i++)
+	{
+		cout<<(char) i;
+	}
 }
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	display();
 	Calculator();
+	Sizes();
+	Characters();
 	return 0;
 }
