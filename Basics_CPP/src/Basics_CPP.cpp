f28cbc558ba8b4e37ef623fa602b5f226a2e2835
chinni <chinni@DESKTOP-88OO4MJ>
2019-09-01 14:01:59 +0200

    Added 2 methods Calling a method from main Simple Calculator operation using Local variables

--- Basics_CPP/src/Basics_CPP.cpp
+++ Basics_CPP/src/Basics_CPP.cpp
@@ -9,7 +9,25 @@
 #include <iostream>
 using namespace std;
 
+void display()
+{
+	cout<<"\nHI.. I am a display method called from Main method\n";
+}
+
+void Calculator()
+{
+	int a=8,b=3;
+	cout<<"\n\nSample Calculator DEMO\n";
+	cout<<"SUM of "<<a<<" and "<<b<<" is : \t"<<(a+b);
+	cout<<"\nDIFFERENCE of "<<a<<" and "<<b<<" is : \t"<<(a-b);
+	cout<<"\nPRODUCT of "<<a<<" and "<<b<<" is : \t"<<(a*b);
+	cout<<"\nDIFFERENCE of "<<a<<" and "<<b<<" is : \t"<<((float)(a/b));
+
+}
+
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	display();
+	Calculator();
 	return 0;
 }
