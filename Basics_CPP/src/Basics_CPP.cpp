6856f0894294263e186acd69e6b180faaf374f6a
chinni <chinni@DESKTOP-88OO4MJ>
2019-09-01 13:49:12 +0200

    Basics-CPP. First commit

---
+++ Basics_CPP/src/Basics_CPP.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Basics_CPP.cpp
+// Author      : Subhash SRIVATSA
+// Version     :
+// Copyright   : @ Programming
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
